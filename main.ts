978ead32d953bfd3ce6703619b5f58857469c264
Todor Trundev <ttrundev@visteon.com>
2020-01-14 18:09:02 +0200

    Add "blocks" (//%) tags and a main TS file

---
+++ main.ts
@@ -0,0 +1,6 @@
+function main_ts() {
+    basic.showString("Hello from TS!");
+    test_cpp.test_cpp();
+}
+
+main_ts();
--- main_cpp.cpp
+++ main_cpp.cpp
@@ -1,11 +1,26 @@
-#include "pxt.h"
+//#include "pxt.h"
 
-namespace TestMethods
+enum test_enum {
+    //% blockIdentity="test_cpp.test_val0"
+    test_val0,
+    //% blockIdentity="test_cpp.test_val1"
+    test_val1,
+    //% blockIdentity="test_cpp.test_val2"
+    test_val2
+};
+
+
+//% weight=1 color="#333333"
+//% advanced=true
+namespace test_cpp
 {
 
-void test_cpp()
+//% weight=30 async
+//% blockId="test_fn" block="Test C++"
+int test_fn()
 {
-    pxt::uBit.display.scroll("Hello from C++!");
+    //pxt::uBit.display.scroll("Hello from C++!");
+    return 123; 
 }
 
 }
