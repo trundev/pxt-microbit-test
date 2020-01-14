cafc56eeea3f937db929379eec6d8c44a46607ea
Todor Trundev <ttrundev@visteon.com>
2020-01-14 12:56:21 +0200

    Experimental serial read from Qbit's base-module

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+basic.showString("Qbit ser");
+serial.redirect(SerialPin.P12, SerialPin.P8, 115200)
+
+serial.onDataReceived("$", function () {
+    let str = serial.readUntil("$");
+    basic.showString(str);
+})
