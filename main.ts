167fcaeb459b7c5386f54841899a1a4f2582830a
Todor Trundev <ttrundev@visteon.com>
2020-01-14 13:07:57 +0200

    Empty blocks ans TS files

--- main.ts
+++
@@ -1,7 +0,0 @@
-basic.showString("Qbit ser");
-serial.redirect(SerialPin.P12, SerialPin.P8, 115200)
-
-serial.onDataReceived("$", function () {
-    let str = serial.readUntil("$");
-    basic.showString(str);
-})
