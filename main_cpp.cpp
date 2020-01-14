379df7c4369c0f781713e22ba698e60cd523e4ba
Todor Trundev <ttrundev@visteon.com>
2020-01-14 15:22:41 +0200

    Add shims.d.ts

--- main_cpp.cpp
+++ main_cpp.cpp
@@ -1,11 +1,11 @@
 #include "pxt.h"
 
+namespace TestMethods
+{
+
 void test_cpp()
 {
     pxt::uBit.display.scroll("Hello from C++!");
 }
 
-void main()
-{
-    test_cpp();
 }
---
+++ shims.d.ts
@@ -0,0 +1,6 @@
+declare interface Test {
+    
+    //% shim=TestMethods::test_cpp
+    //% parts="Test"
+    test_cpp(): void;
+}
